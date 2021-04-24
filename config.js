89da6399d74684d327b15f92a5d63e615f0474df
Sarthak Bhatt <sarthaksfolder@Sarthaks-MacBook-Pro.local>
2021-04-24 00:07:37 -0400

    first test

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 module.exports = {
   database:
     "mongodb+srv://admin:admin@studentrecordcloud.svbk7.mongodb.net/furnitureStore?retryWrites=true&w=majority",
-  port: 3030,
+  port: process.env.PORT || 3030,
   secret: "WPL_FINAL_PROJECT",
 };
