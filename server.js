ce9d53ecb3c454a072c5651eb5e0dcdd7d509806
Sarthak Bhatt <sarthakbhatt16@gmail.com>
2021-04-24 20:20:22 -0400

    test seven

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DETELE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
   res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
